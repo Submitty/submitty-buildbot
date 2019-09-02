c25c127ee9dff72dd069383773912980cc65e0b1
fenn-cs <fenn25.fn@gmail.com>
2019-09-02 01:17:20 +0100

    renamed php-worker and its references
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- workers/php/php_syntax_lint.py
+++ workers/php/php_syntax_lint.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-from os import system
-php_syntax_lint_command = "find -L . -path ./site/vendor -prune -o -name \*.php -print0 | xargs -0 -n 1 -P 4 php -l"
-system(php_syntax_lint_command)
+from subprocess import check_call
+php_syntax_lint_command = ["find -L . -path ./site/vendor -prune -o -name \*.php -print0 | xargs -0 -n 1 -P 4 php -l"]
+check_call(php_syntax_lint_command, shell=True)
